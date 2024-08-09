f0941531f6d03bd2edcd30424d0176baabe83f6f
Matias Martinez <martinez_matias@live.com.ar>
2024-08-09 02:14:58 -0300

    details fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import authRouter from './src/routes/auth.routes.js';
-import memberRouter from './src/routes/members.routes.js';
+import membersRouter from './src/routes/members.routes.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -20,4 +20,4 @@ app.use(cors({
 
 app.use(express.json());
 app.use("/api/v1", authRouter);
-app.use("/api/v1", memberRouter); // Añadir la ruta de miembros
+app.use("/api/v1", membersRouter); // Añadir la ruta de miembros
